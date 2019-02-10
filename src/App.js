d6bbf534e1a84c7ce5d95129cd45d5210b1d53a7
Raghav Jajodia <jajodia.raghav@gmail.com>
2019-02-11 00:52:28 +0530

    FAQ: Fixed stylings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./components/Landing";
-import GetStarted from "./components/GetStarted/index";
-import Faq from "./components/Faq-page/faq"
+import GetStarted from "./components/GetStarted";
+import FAQ from "./components/FAQ"
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/faq" component={Faq} />
+        <Route path="/faq" component={FAQ} />
         <Route path="/mentors" />
         <Route path="/contact" />
         <Route path="/login" />
