ca6ce4f387e24cade524b0e75fd48ca0e6fb6135
shilpi verma <sverma@nobul.com>
2019-09-15 22:24:46 +0530

    Add chatbot window

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch,Route,BrowserRouter as Router} from "react-router-dom"
+import Login from './components/Login';
+import HomePage from "./components/HomePage";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={HomePage} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
+import {createStore,applyMiddleware} from 'redux';
+import {Provider} from "react-redux";
+import reducers from './reducer/index';
+import ReduxPromise from "redux-promise";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState={}
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+    <Provider  store={createStoreWithMiddleware	(reducers,initialState)}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+    </Provider>
+		, document.getElementById('root')
+	);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
